7aacb2ab291f895afe6084c251465d7787b5361d
PragyanPandeyX <107480064+PragyanPandeyX@users.noreply.github.com>
2024-11-13 00:46:01 +0530

    Update configs.py

--- pyPandey/configs.py
+++ pyPandey/configs.py
@@ -1,53 +1,55 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
 import sys
-import os
+
+from decouple import config
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
 
 class Var:
-    # Mandatory configurations
+    # mandatory
     API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else 20348897
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
     )
     API_HASH = (
         sys.argv[2]
         if len(sys.argv) > 2
-        else "ecfb9a700b8398caf58c53f36ccf5b06"
+        else config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
     )
-    SESSION = (
-        sys.argv[3] if len(sys.argv) > 3 
-        else "BQE2f-EACfbwubzRBTsNMBEjZ7LxEateLwOimujk-kPaeiLGt1hgQJngckWG3LVm3UjlPFiOOF39kD0SS-3oeMkUmoiYOXg99eJuywiagk2TmRVd_yHPpaxhf_iabR8Q4sjGjy2ByiKUJU3jp3jI90fqoywckNdENn_EVudx0vvZ7tAYIuI1-U8L691Yd22aeLg3GkQwVVJ22Jq760zsAyKH8eecYEgM3kA7L_PHk4phoVLym0eIuWC7a4KmqQBcrTD4-VeN7gsQsA8omnjlKu7pDsvUEjrwVOIyeUPC_B3IDWRHXX0eSZOwDeoKOGPrOWkaRkNQkE9pomPOodeUUOVYaDw8TwAAAAHdPGQXAA"
-    )
+    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
     REDIS_URI = (
-        sys.argv[4] if len(sys.argv) > 4 else None  # Set a default if you have one
+        sys.argv[4]
+        if len(sys.argv) > 4
+        else (config("REDIS_URI", default=None) or config("REDIS_URL", default=None))
     )
     REDIS_PASSWORD = (
-        sys.argv[5] if len(sys.argv) > 5 else None  # Set a default if you have one
+        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=None)
     )
-
-    # Extras
-    
-    LOG_CHANNEL = -1002377046364  # Set log channel here
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    HEROKU_API = os.environ.get("HEROKU_API")
-    VC_SESSION = os.environ.get("VC_SESSION")
-    ADDONS = os.environ.get("ADDONS", 'True').lower() in ['true', '1']
-    VCBOT = os.environ.get("VCBOT", 'False').lower() in ['true', '1']
-    
-    # For Railway
-    REDISPASSWORD = os.environ.get("REDISPASSWORD")
-    REDISHOST = os.environ.get("REDISHOST")
-    REDISPORT = os.environ.get("REDISPORT")
-    REDISUSER = os.environ.get("REDISUSER")
-    
-    # For SQL
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    
-    # For MongoDB
-    MONGO_URI = "mongodb+srv://AvyukthX:AvyukthX@bot.ctdiudr.mongodb.net/?retryWrites=true&w=majority"
-
-# Example usage
-if __name__ == "__main__":
-    print(f"API_ID: {Var.API_ID}")
-    print(f"API_HASH: {Var.API_HASH}")
-    print(f"SESSION: {Var.SESSION}")
-    print(f"BOT_TOKEN: {Var.BOT_TOKEN}")
-    print(f"MONGO_URI: {Var.MONGO_URI}")
+    # extras
+    BOT_TOKEN = config("BOT_TOKEN", default=None)
+    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
+    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+    HEROKU_API = config("HEROKU_API", default=None)
+    VC_SESSION = config("VC_SESSION", default=None)
+    ADDONS = config("ADDONS", default=False, cast=bool)
+    VCBOT = config("VCBOT", default=False, cast=bool)
+    # for railway
+    REDISPASSWORD = config("REDISPASSWORD", default=None)
+    REDISHOST = config("REDISHOST", default=None)
+    REDISPORT = config("REDISPORT", default=None)
+    REDISUSER = config("REDISUSER", default=None)
+    # for sql
+    DATABASE_URL = config("DATABASE_URL", default=None)
+    # for MONGODB users
+    MONGO_URI = config("MONGO_URI", default=None)
